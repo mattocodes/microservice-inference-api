7eb8cf5dcc221fac7a22eef3df64a33ece7a1b49
mattocodes <39041501+mattocodes@users.noreply.github.com>
2020-05-27 00:29:10 +0000

    Update the run_kubernetes.sh file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mattocodes/prediction:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run myapp\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=myapp
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward myapp 8000:80
 
